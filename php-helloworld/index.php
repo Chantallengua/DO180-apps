2f9973ec164768d7fc8c02ac44b606809273a187
Student User <student@workstation.lab.example.com>
2023-01-28 06:15:36 -0500

    updated appina

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "AAAAAAAAA change is in the air "  "\n";
+print "Hello, World! Ancora e di nuovo e basta. php version is " . PHP_VERSION . "\n";
 ?>
